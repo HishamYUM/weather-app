f40167dd8bef33a409f5ecd61af295e73ed54eca
HishamYum <invictus0maneo@gmail.com>
2022-11-15 17:02:08 +0100

    push to github

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+		"X-RapidAPI-Key": "1b2876ade9mshbcd05cc6a5c8759p1f0088jsn97d218f456d3",
+		"X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f52a7cf0eccbf3af41425f62bdcd8d9a";
